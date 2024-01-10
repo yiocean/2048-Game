0edb1627c02243c167f4b5cf28aa87921eaf8638
yiocean <58471668+yiocean@users.noreply.github.com>
2024-01-10 19:38:50 +0800

    0110 commit

--- Train/playByTuple.py
+++ Train/playByTuple.py
@@ -5,135 +5,78 @@ import copy
 import os
 import openpyxl
 import csv
+import time
+import datetime
 
 if __name__ == "__main__":
 
-    #creat Excel
+    currentDatetime = datetime.datetime.now()
+    formattedDatetime = currentDatetime.strftime("%m%d-%H%M%S")
+    filename = f"Tuple_{formattedDatetime}.xlsx"
+    filename1 = f"out_{formattedDatetime}.txt"
+
+    #creat Excel -> record results（win or lose）
     exl = openpyxl.Workbook()
-    exl.save('Tuple2.xlsx')
-    exl = openpyxl.load_workbook('Tuple2.xlsx') 
+    exl.save(filename)
+    exl = openpyxl.load_workbook(filename) 
     sheet1 = exl['Sheet']
     sheet1.cell(1, 1).value = 'Game number' #(row, col) = (1, 1)
     sheet1.cell(1, 2).value = 'Win or lose'
     sheet1.cell(1, 3).value = 'Final Score'
-    exl.save('Tuple2.xlsx')
+    exl.save(filename)
 
-    #creat csv file -> record step
-    row = ['current num', 'win lose continue','cur score', 'chose', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'reward']
-    # win = 1, lose = -1, continue = 0
-    csvfile = open('step3.csv', 'w', newline='')
-    csvfile = csv.writer(csvfile)
-    csvfile.writerow(row)
+    # record results（% of tiles）
+    filenameOpen = open(filename1, 'w',newline='')
     
     ntuple = Tuple()
+    game = Board()
     
     winCot = 0
     loseCot = 0
-    #highestScore = 0
-    #playCot = 0 # record how many board had played
-    #n = 0
-    #list = ["U", "R", "D", "L"]
+
     list = ["R", "D", "L", "U"]
 
-    k = 1000 #episode
+    k = 3000 #episode
+    milestone = 100
     final = np.zeros((k, 3), dtype = np.uint32)
     score200 = []
     score200sum = 0
+
+    totalReward = 0
     
+    num_games = 0
+    totla_score = 0
     for j in range(k):
         original_tuple = np.copy(ntuple.tuple)
-        game = Board()
-        winFlag = 0
+        game.startNewGame()
         score = 0
-        cin_flag = -1
-        #record init board
-        row = [j, 0, score, 'init board'] + [game.board[i][j] for i in range(4) for j in range(4)] + [0]
-        csvfile.writerow(row)
-        #final[i][0] = j
+        cin_flag = 0
         while True:
-            #score, cin_flag, n = game.checkWinLoseContinue(game, ntuple, cin_flag, score, n)
-            tmp = Board()
-            tmax = -1
-            irecord = -1
-            scorelist = [0, 0, 0, 0]
-            r = 0
-
-            # find max step
-            for i in range(4):
-                tmp.board = game.board
-                m, tmp.board = tmp.move(list[i]) # m = reward
-               # tmp.board = boardNotUse #check
-                scorelist[i] = m
-                checksum = tmp.check()
-                if checksum == -5:
-                    continue
-                elif checksum == 100:
-                    winFlag = 1
-                    irecord = i
-                    break
-                else:
-                    if m != -5:
-                        tmp.newTile()
-                        t = ntuple.getTupleValueSum(tmp.board) + m
-                        if t > tmax:
-                            tmax = t
-                            irecord = i
+            tmp_board = game.board
+            getT1 = ntuple.getTupleValueSum(game.board)            
+            r, game.board = ntuple.takeAction(list, game)
+            totalReward += r
+            score += r
+            #aferstate = game.board()
+            game.newTile()
+            getT2 = ntuple.getTupleValueSum(game.board)
+            ntuple.udpTuple(tmp_board, r+(getT2 - getT1))
+            if game.check() == 100:
+                cin_flag = 1
+            if game.endOfGame() == True:
+                break            
+        # record result (% of tiles)
+        totla_score += score
+        num_games += 1     
+        if num_games % 10 == 0:
+            print("avg_score: ", totla_score/10)
+            totla_score = 0
             
-            #get tuple 1
-            getT1 = ntuple.getTupleValueSum(game.board)
-            if winFlag == 1:
-                print("win")
-                r, game.board = game.move(list[i])
-                game.newTile()
-                getT2 = ntuple.getTupleValueSum(game.board)
-                score += r
-                cin_flag = 1 #win or lose
+        ntuple.addCount(game)  
 
-                # more than 2048 (more than 16384 -> 16384)
-                ntuple.udpTuple(game.board, r + (getT2 - getT1))
-                row = [j, 1, score, list[irecord]] + [game.board[i][j] for i in range(4) for j in range(4)]
-                #csvfile.writerow(row)
-                break
-            elif tmax == -1: #四個方向都不能走 -> 遊戲結束
-                print("lose")
-                maxscore = -1
-                irecordm = -1
-                for w in range(4): #找score最大的走
-                    if scorelist[w] > maxscore:
-                        maxscore = scorelist[w]
-                        irecordm = w
-                r, game.board = game.move(list[irecordm])
-                score += r
-                cin_flag = 0
-                row = [j, -1, score, 'no move'] + [game.board[i][j] for i in range(4) for j in range(4)]
-                break
-            else:
-                r, tboard = game.move(list[irecord])
-                game.board = tboard
-                #get tuple 2
-                
-                score += r
-                if r != -5:
-                    game.newTile()
-                
-                getT2 = ntuple.getTupleValueSum(game.board)
-                row = [j, 0, score, list[irecord]] + [game.board[i][j] for i in range(4) for j in range(4)]
-                #game.printBoard()
-                #print("Score: ", score)
-            
-            row = row + [r]
-            ntuple.udpTuple(game.board, r + (getT2 - getT1))
-            csvfile.writerow(row)
-
-            # test if tuple value change
-            # changed_indices = np.where(ntuple.tuple != original_tuple)
-            # for r, c in zip(*changed_indices):
-            #     print(f"position ({r}, {c}) 's value from {original_tuple[r, c]} change to {ntuple.tuple[r, c]}")             
-            
-
-        game.printBoard()
-        print("Score: ", score)
-        print("k: ", j)
+        # game.printBoard()
+        # print("Score: ", score)
+        # print("k: ", j)
         
         if cin_flag == 0:
             loseCot += 1
@@ -144,22 +87,26 @@ if __name__ == "__main__":
         final[j][1] = cin_flag
         final[j][2] = score
         
+        # save win or lose
         sheet1.cell(j+2, 1).value = j
         sheet1.cell(j+2, 2).value = cin_flag
         sheet1.cell(j+2, 3).value = score
         exl.save('Tuple2.xlsx')
 
-        score200sum += score
-        if j % 200 == 0:
-            score200.append(score200sum / j)
-            score200sum = 0
+        # show results
+        if ((j+1) % milestone) == 0:
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+            print("#Episode: {episode}, score: {score}".format(episode = j+1, score = totalReward // milestone ))
+            line = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+            filenameOpen.write(line)
+            line = "#Episode: {episode}, score: {score}\n".format(episode = j+1, score = totalReward // milestone )
+            filenameOpen.write(line)
+            ntuple.saveTuple()
+            ntuple.printCount(milestone, filenameOpen)
+            ntuple.resetCount()
+            totalReward = 0
     
     print(winCot / k)
     print(final)
-    sheet1.cell(k+5, 1).value = (winCot / k)
-    exl.save('Tuple2.xlsx')
-    # print(score200sum)
-    print(score200)
-        #exl.save
-
-    #csvfile.close()
+    ntuple.saveTuple()
+    exl.close()
--- Train/tuple.py
+++ Train/tuple.py
@@ -3,22 +3,40 @@ import numpy as np
 import random
 import os
 import math
+import time
 
 class Tuple():
     def __init__(self):
-        # self.episode = []
-        # self.net = []
+        self.tuple = []
         self.alpha = 0.0025
         self.gamma = 1.0
-        # self.resetCount()
+        self.maxtile = 0
+        self.resetCount()
 
-        self.buildTuple()
+        if os.path.isfile("tupleNet/tuple1.npy"):
+            print("Found tuple")
+            self.loadTuple("tupleNet/tuple")
+            print("Completed")
+        else:
+            print("Building tuple")
+            self.buildTuple()
+            print("Completed")
 
     def buildTuple(self):
         #self.tuple = np.random.uniform(0, 1, (17, 50625))
         self.tuple = np.zeros((17, 50625))
         #self.tuple = np.zeros((17))
         self.value = np.zeros((17))
+
+    def loadTuple(self, filename):
+        self.value = np.zeros((17))
+        for i in range(17):
+            self.tuple.append(np.load(filename+str(i+1)+".npy"))
+            print(self.tuple[i])
+
+    def saveTuple(self):
+        for i in range(17):
+            np.save("tupleNet/tuple%d" % (i+1), self.tuple[i])
 
     def findPowerOfTwo(self, number):
         power = math.log(number, 2)
@@ -48,34 +66,32 @@ class Tuple():
             d = d1
 
         sumnum = a + b * 15 + c * 15 * 15 + d * 15 * 15 * 15
-        return int(sumnum)
-        #return min(max(sumnum, 0), 50624)
+        return min(int(sumnum), 50625)
     
     def getTupleValueSum(self, board):
-        sumnum = 0
+        sumnum = 0.0
+        for i in range(17):
+            self.value[i] = 0
+        
         self.value[0] = self.tuple[0][self.cot16(board[0][0], board[0][1], board[1][0], board[1][1])]
         self.value[1] = self.tuple[1][self.cot16(board[0][2], board[0][3], board[1][2], board[1][3])]
         self.value[2] = self.tuple[2][self.cot16(board[2][0], board[2][1], board[3][0], board[3][1])]
         self.value[3] = self.tuple[3][self.cot16(board[2][2], board[2][3], board[3][2], board[3][3])]
         for i in range(4):
-            self.value[i+3] = self.tuple[i+3][self.cot16(board[i][0], board[i][1], board[i][2], board[i][3])]
+            self.value[i+4] = self.tuple[i+4][self.cot16(board[i][0], board[i][1], board[i][2], board[i][3])]
         for i in range(4):
-            self.value[i+7] = self.tuple[i+7][self.cot16(board[0][i], board[1][i], board[2][i], board[3][i])]
+            self.value[i+8] = self.tuple[i+8][self.cot16(board[0][i], board[1][i], board[2][i], board[3][i])]
         self.value[12] = self.tuple[12][self.cot16(board[0][1], board[0][2], board[1][1], board[1][2])]
         self.value[13] = self.tuple[13][self.cot16(board[1][0], board[1][1], board[2][0], board[2][1])]
         self.value[14] = self.tuple[14][self.cot16(board[1][1], board[1][2], board[2][1], board[2][2])]
         self.value[15] = self.tuple[15][self.cot16(board[1][2], board[1][3], board[2][1], board[2][2])]
         self.value[16] = self.tuple[16][self.cot16(board[2][1], board[2][2], board[3][1], board[3][2])]
 
-        # for i in range(17):
-        #     if self.value[i] > 50625
-
         for i in range(17):
-            sumnum += self.tuple[i][int(self.value[i])]
+            sumnum += self.value[i]
 
         return sumnum
     
-    #def findMaxStep()
     def udpTuple(self, board, difference):
         tmpr = difference * self.alpha
         self.tuple[0][self.cot16(board[0][0], board[0][1], board[1][0], board[1][1])] += tmpr
@@ -83,30 +99,14 @@ class Tuple():
         self.tuple[2][self.cot16(board[2][0], board[2][1], board[3][0], board[3][1])] += tmpr
         self.tuple[3][self.cot16(board[2][2], board[2][3], board[3][2], board[3][3])] += tmpr
         for i in range(4):
-            self.tuple[i+3][self.cot16(board[i][0], board[i][1], board[i][2], board[i][3])] += tmpr
+            self.tuple[i+4][self.cot16(board[i][0], board[i][1], board[i][2], board[i][3])] += tmpr
         for i in range(4):
-            self.tuple[i+7][self.cot16(board[0][i], board[1][i], board[2][i], board[3][i])] += tmpr
+            self.tuple[i+8][self.cot16(board[0][i], board[1][i], board[2][i], board[3][i])] += tmpr
         self.tuple[12][self.cot16(board[0][1], board[0][2], board[1][1], board[1][2])] += tmpr
         self.tuple[13][self.cot16(board[1][0], board[1][1], board[2][0], board[2][1])] += tmpr
         self.tuple[14][self.cot16(board[1][1], board[1][2], board[2][1], board[2][2])] += tmpr
         self.tuple[15][self.cot16(board[1][2], board[1][3], board[2][1], board[2][2])] += tmpr
         self.tuple[16][self.cot16(board[2][1], board[2][2], board[3][1], board[3][2])] += tmpr
-
-    
-    # def getTupleValue(self, board):
-    #     self.tuple[0] = self.cot16(board[0][0], board[0][1], board[1][0], board[1][1])
-    #     self.tuple[1] = self.cot16(board[0][2], board[0][3], board[1][2], board[1][3])
-    #     self.tuple[2] = self.cot16(board[2][0], board[2][1], board[3][0], board[3][1])
-    #     self.tuple[3] = self.cot16(board[2][2], board[2][3], board[3][2], board[3][3])
-    #     for i in range(4):
-    #         self.tuple[i+3] = self.cot16(board[i][0], board[i][1], board[i][2], board[i][3])
-    #     for i in range(4):
-    #         self.tuple[i+7] = self.cot16(board[0][i], board[1][i], board[2][i], board[3][i])
-    #     self.tuple[12] = self.cot16(board[0][1], board[0][2], board[1][1], board[1][2])
-    #     self.tuple[13] = self.cot16(board[1][0], board[1][1], board[2][0], board[2][1])
-    #     self.tuple[14] = self.cot16(board[1][1], board[1][2], board[2][1], board[2][2])
-    #     self.tuple[15] = self.cot16(board[1][2], board[1][3], board[2][1], board[2][2])
-    #     self.tuple[16] = self.cot16(board[2][1], board[2][2], board[3][1], board[3][2])
     
         
     def printTuple(self, board):
@@ -114,22 +114,27 @@ class Tuple():
         for i in range(17):
             print("The number of tuple", i, " is ", self.tuple[i])
 
-    
-    def getValue(self, board):
-        self.value[0] = board[0][0] + board[0][1] + board[1][0] + board[1][1]
-        self.value[1] = board[0][2] + board[0][3] + board[1][2] + board[1][3]
-        self.value[2] = board[2][0] + board[2][1] + board[3][0] + board[3][1]
-        self.value[3] = board[2][2] + board[2][3] + board[3][2] + board[3][3]
+    def takeAction(self, list, pre):
+        irecord = -1
+        tmax = -10000
+        tmp = Board()
         for i in range(4):
-            self.value[i+3] = board[i][0] + board[i][1] + board[i][2] + board[i][3]
-        for i in range(4):
-            self.value[i+7] = board[0][i] + board[1][i] + board[2][i] + board[3][i]
-        self.value[12] = board[0][1] + board[0][2] + board[1][1] + board[1][2]
-        self.value[13] = board[1][0] + board[1][1] + board[2][0] + board[2][1]
-        self.value[14] = board[1][1] + board[1][2] + board[2][1] + board[2][2]
-        self.value[15] = board[1][2] + board[1][3] + board[2][1] + board[2][2]
-        self.value[16] = board[2][1] + board[2][2] + board[3][1] + board[3][2]
+            tmp.copyBoard(pre)
+            r, tmp.board = tmp.move(list[i])
+            if r != -5:
+                # tmp.newTile()
+                t = r + self.getTupleValueSum(tmp.board) 
+                if t > tmax:
+                    tmax = t
+                    irecord = i
 
+        if irecord != -1:
+            tmp.copyBoard(pre)
+            r, cur = tmp.move(list[irecord])
+            return r, cur
+        else:
+            return -1, pre.board
+    
     def printValue(self, board):
         valuesum = 0
         self.getValue(board)
@@ -137,3 +142,30 @@ class Tuple():
             valuesum += self.value[i]
         print("sum value is ", valuesum)
         return valuesum
+    
+    # record % of tiles
+    def resetCount(self):
+        self.maxtile = 0
+        self.count = [0 for i in range(25)]
+    
+    def addCount(self,b):
+        mt = 0
+        for pos in range(16):
+            r = pos // 4
+            c = pos % 4
+            t = b.board[r][c]
+            if t != 0:
+                t = int(self.findPowerOfTwo(t))
+            if t > mt :
+                mt = t
+            if mt > self.maxtile :
+                self.maxtile = mt
+        for idx in range(1, mt + 1):
+            self.count[idx] += 1
+
+    
+    def printCount(self, milestone, file):
+        for i in range(self.maxtile, max(self.maxtile-6,1), -1):
+            print("{}: {:.2%}".format((np.int32(1) << np.int32(i)), (self.count[i] / milestone)))
+            line = "{}: {:.2%}\n".format((np.int32(1) << np.int32(i)), (self.count[i] / milestone))
+            file.write(line)
